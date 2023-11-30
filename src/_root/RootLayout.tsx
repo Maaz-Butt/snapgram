66c8929e020611bf002c37f27cfde2d979696e4c
Maaz-Butt <maazb7461@gmail.com>
2023-12-01 00:19:02 +0500

    first commit

---
+++ src/_root/RootLayout.tsx
@@ -0,0 +1,21 @@
+import Bottombar from '@/components/ui/shared/Bottombar'
+import Leftsidebar from '@/components/ui/shared/Leftsidebar'
+import Topbar from '@/components/ui/shared/Topbar'
+import { Outlet } from 'react-router-dom'
+
+const RootLayout = () => {
+  return (
+    <div className='w-full md:flex'>
+      <Topbar />
+      <Leftsidebar />
+
+      <section className='flex flex-1 h-full'>
+        <Outlet />
+      </section>
+
+      <Bottombar />
+    </div>
+  )
+}
+
+export default RootLayout
---
+++ src/_root/pages/Explore.tsx
@@ -0,0 +1,67 @@
+import GridPostList from "@/components/ui/shared/GridPostList";
+import Loader from "@/components/ui/shared/Loader";
+import useDebounce from "@/hooks/useDebounce";
+import {
+  useGetPosts,
+  useSearchPosts,
+} from "@/lib/react-query/queriesAndMutations";
+import { useState, useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+
+const Explore = () => {
+  const { ref, inView } = useInView();
+  const { data: posts, fetchNextPage, hasNextPage } = useGetPosts();
+
+  const [searchValue, setSearchValue] = useState("");
+  const debounceValue = useDebounce(searchValue, 500);
+  const { data: searchedPosts, isFetching: isSearchFetching } =
+    useSearchPosts(debounceValue);
+
+  useEffect(() => {
+    if (inView && !searchValue) fetchNextPage();
+  }, [inView, searchValue]);
+
+  if (!posts) {
+    return (
+      <div className="flex-center w-full h-full">
+        <Loader />
+      </div>
+    );
+  }
+
+  const shouldShowSearchResults = searchValue !== "";
+  const shouldShowPosts =
+    !shouldShowSearchResults &&
+    posts.pages.every((item) => item.documents.length === 0);
+
+  return (
+    <div className="explore-container">
+      <div className="flex-between w-full max-w-5xl mt-16 mb-7">
+        <h3 className="body-bold md:h3-bold">Popular Today</h3>
+      </div>
+
+      <div className="flex flex-wrap gap-9 w-full max-w-5xl">
+        {shouldShowSearchResults ? (
+          <SearchResults
+            isSearchFetching={isSearchFetching}
+            searchedPosts={searchedPosts}
+          />
+        ) : shouldShowPosts ? (
+          <p className="text-light-4 mt-10 text-center w-full">End of posts</p>
+        ) : (
+          posts.pages.map((item, index) => (
+            <GridPostList key={`page-${index}`} posts={item.documents} />
+          ))
+        )}
+      </div>
+
+      {hasNextPage && !searchValue && (
+        <div ref={ref} className="mt-10">
+          <Loader />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Explore;
